c2b356532bfa4cfc231f1c6634ead2a13ac6ceeb
yahrMason <yahr.mason@gmail.com>
2023-06-14 14:58:39 -0500

    adding code to adjust img path in markdown

--- _py/ipynb_to_md.py
+++
@@ -1,7 +0,0 @@
-import os
-
-NOTEBOOK = "2023-06-14-ode-numeric-approx"
-
-if __name__ == "__main__":
-    os.system(f"jupyter nbconvert --to markdown {NOTEBOOK}.ipynb")
-    os.replace(f"{NOTEBOOK}.md", f"../_posts/{NOTEBOOK}.md")
